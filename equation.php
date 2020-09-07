36c30860657cfebc0c840aef51aafbd07030ce67
Ma6upa <67150070+Ma6upa@users.noreply.github.com>
2020-09-07 21:57:44 +0400

    Initial commit

---
+++ equation.php
@@ -0,0 +1,22 @@
+<?php
+    function line($a, $b){
+        if (is_numeric($a) && is_numeric($b)){
+            if ($a != 0){
+                return[-$b / $a];
+            }
+        }
+        return[];
+    }
+    function square($a, $b, $c){
+        if(is_numeric($a) && is_numeric($b) &&  is_numeric($c)){
+            if($a == 0){
+                return line($b, $c);
+            }
+            $d = $b * $b - 4 * $a * $c;
+            if($d >= 0){
+                return[(-$b + sqrt($d))/(2 * $a), (-$b - sqrt($d))/(2 * $a)];
+            }
+        }
+        return[];
+    }
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once('equation.php');
+    print_r(line(1,2));
+    print_r(square(1,4,-5));
+?>
